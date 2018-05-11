0b445d855d704f9eda57dc380ef79244c17e07c0
silvinobrigido <silvinobrigido@hotmail.com>
2018-05-11 19:46:33 -0300

    FEAT(CRIADA AS 'CRASSES')

---
+++ src/br/furb/programacaoii/problema3/controller/calculo/CalculoRetiradaLocal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.furb.programacaoii.problema3.controller.calculo;
+
+import br.furb.programacaoii.problema3.model.Pedido;
+
+/**
+ *
+ * @author silvinos
+ */
+public class CalculoRetiradaLocal implements CalculoEntrega {
+
+    public static CalculoEntrega getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Double calcularValorEntrega(Pedido pedido) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/br/furb/programacaoii/problema3/controller/calculo/CalculoSedex.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.furb.programacaoii.problema3.controller.calculo;
+
+import br.furb.programacaoii.problema3.model.Pedido;
+
+/**
+ *
+ * @author silvinos
+ */
+public class CalculoSedex implements CalculoEntrega {
+
+    public static CalculoEntrega getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Double calcularValorEntrega(Pedido pedido) {
+        return null;
+        
+    }
+    
+}
